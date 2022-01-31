692db944f1d7d15d1259993d03338974099c71c0
Jaswal1p <Jaswal1p@yahoo.com>
2022-01-31 14:17:04 -0500

    successful npm start with localhost showing json

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { animals } = require('./data/animals');
+
+const app = express();
+
+
+
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+});
+
+app.get('/api/animals', (req, res) => {
+    res.json(animals);
+});

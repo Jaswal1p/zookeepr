33149642f36b3a2b899f462061df97c51ae14150
Jaswal1p <Jaswal1p@yahoo.com>
2022-02-08 14:57:58 -0500

    added createNewAnimal function and added id method in post

--- server.js
+++ server.js
@@ -55,6 +55,14 @@ function findById(id, animalsArray) {
   return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+  console.log(body);
+  // our function's main code will go here!
+
+  // return finished code to post route for response
+  return body;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -73,8 +81,9 @@ app.get('/api/animals/:id', (req, res) => {
 });
 
 app.post('/api/animals', (req, res) => {
-  // req.body is where our incoming content will be
-  console.log(req.body);
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+  
   res.json(req.body);
 });
 

fddca00cc4f8e277c5ebd4b04be10e530443bb9c
Jaswal1p <Jaswal1p@yahoo.com>
2022-02-09 20:28:01 -0500

    final codes for get requests

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -28,24 +28,24 @@ const handleAnimalFormSubmit = event => {
   const animalObject = { name, species, diet, personalityTraits };
 
 
-fetch('/api/animals', {
-  method: 'POST',
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(animalObject)
-})
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    }
-    alert('Error: ' + response.statusText);
+  fetch('/api/animals', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(animalObject)
   })
-  .then(postResponse => {
-    console.log(postResponse);
-    alert('Thank you for adding an animal!');
-  });
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      alert(`Error: ${response.statusText}`);
+    })
+    .then(postResponse => {
+      console.log(postResponse);
+      alert('Thank you for adding an animal!');
+    });
 
 };
 
